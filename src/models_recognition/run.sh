3d9838a5ced7ffad934b26a29472d83667df8b22
kinironote <kinironote@gmail.com>
2018-02-17 10:55:38 +0900

    [add] Add update language model

---
+++ src/models_recognition/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# $1 ... データ番号[1-7]
+# $2 ... モード[1-3]
+
+DIST="./data/processed/csj/$1"
+LANG_MODEL_PATH="./data/external/csj.pdp.htkdic.utf8" #utf8?
+
+CONFIG_NAME="clustering-50-n-SVM-0.1"
+MODE=$2
+
+DISTANCE_PATH=$DIST/$CONFIG_NAME
+UPDATED_LANG_MODEL_PATH=$DIST/"model-"$MODE"-"$CONFIG_NAME
+
+# 5. 言語モデルの更新
+echo "Update language model"
+# コンパイルは一回だけでいい…
+g++ -std=c++17 -Wall -O2 src/models_recognition/update_langmodel.cpp -o ./data/processed/update_langmodel
+./data/processed/update_langmodel $DISTANCE_PATH $LANG_MODEL_PATH $UPDATED_LANG_MODEL_PATH $DIST $MODE
+nkf -s $UPDATED_LANG_MODEL_PATH > $UPDATED_LANG_MODEL_PATH.sjis
