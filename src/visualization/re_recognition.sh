bd419263075f530c42a887a6c6bd9fc1ec4273cf
kinironote <kinironote@gmail.com>
2018-02-17 10:56:04 +0900

    [add] Add re recognition

---
+++ src/visualization/re_recognition.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# re_recognition.sh [Number(1-7)]
+
+DIST="data/processed/csj/$1"
+NAME="model-2-clustering-50-n-SVM-0.1.sjis"
+
+
+echo "-v $NAME" > $DIST"/updated-"$NAME".jconf"
+
+echo "Re recognitoin with Julius..."
+lib/julius \
+    -C lib/ssr-kit-v4.4.2.1a/main.jconf \
+    -C config/julius_additional.jconf \
+    -C $DIST"/updated-"$NAME".jconf" \
+    -dnnconf config/julius_dnn.dnnconf \
+    -filelist $DIST/filelist.txt \
+    > $DIST"/re_recognition-"$NAME".txt"
