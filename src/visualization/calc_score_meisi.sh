7f04c8479ec50aae861e1aa8f6142d64649a986a
kinironote <kinironote@gmail.com>
2018-03-07 12:56:00 +0900

    meisi change

--- src/visualization/calc_score_meisi.sh
+++ src/visualization/calc_score_meisi.sh
@@ -15,18 +15,18 @@ mecab -O wakati $ANSWER > $ANSWER.wakati  # without | kakasi -JH -i utf8 | nkf -
 mecab -O wakati $RECOGNITION_RESULT_PATH".shaped" > $RECOGNITION_RESULT_PATH".shaped.wakati" # without | kakasi -JH -p -i utf8 | nkf -w
 #mecab -O wakati $RECOGNITION_RESULT_PATH".shaped" > $RECOGNITION_RESULT_PATH".shaped.kanji"
 
-mecab $ANSWER.wakati | grep -e "名詞"| cut -f1 > $ANSWER.wakati.meisi
-mecab $RECOGNITION_RESULT_PATH".shaped.wakati" | grep -e "名詞"| cut -f1 > $RECOGNITION_RESULT_PATH".shaped.wakati.meisi"
+mecab $ANSWER.wakati | grep -v "助詞"| grep -v "助動詞"| grep -v "フィラー"| cut -f1 > $ANSWER.wakati.meisi
+mecab $RECOGNITION_RESULT_PATH".shaped.wakati"  | grep -v "助詞"| grep -v "助動詞"| grep -v "フィラー"|  cut -f1 > $RECOGNITION_RESULT_PATH".shaped.wakati.meisi"
 
 
-python3 src/visualization/calc_score_meisi.py $ANSWER.wakati.meisi $RECOGNITION_RESULT_PATH".shaped.wakati.meisi" $DIST > $DIST/"result_meisi-before-$NAME"
-echo 'save at '$DIST/"result_meisi-before-$NAME"
+python3 src/visualization/calc_score_meisi.py $ANSWER.wakati.meisi $RECOGNITION_RESULT_PATH".shaped.wakati.meisi" $DIST > $DIST/"result_2meisi-before-$NAME"
+echo 'save at '$DIST/"result_2meisi-before-$NAME"
 
 python3 src/visualization/shaping.py $RE_RECOGNITION_RESULT_PATH > $RE_RECOGNITION_RESULT_PATH'.shaped'
 mecab -O wakati $RE_RECOGNITION_RESULT_PATH".shaped" > $RE_RECOGNITION_RESULT_PATH".shaped.wakati" #(ry
 #mecab -O wakati $RE_RECOGNITION_RESULT_PATH".shaped" > $RE_RECOGNITION_RESULT_PATH".shaped.kanji"
 
-mecab $RE_RECOGNITION_RESULT_PATH".shaped.wakati" | grep -e "名詞"| cut -f1 > $RE_RECOGNITION_RESULT_PATH".shaped.wakati.meisi"
+mecab $RE_RECOGNITION_RESULT_PATH".shaped.wakati"  | grep -v "助詞"| grep -v "助動詞"| grep -v "フィラー"|  cut -f1 > $RE_RECOGNITION_RESULT_PATH".shaped.wakati.meisi"
 
-python3 src/visualization/calc_score_meisi.py $ANSWER.wakati.meisi $RE_RECOGNITION_RESULT_PATH".shaped.wakati.meisi" $DIST > $DIST/"result_meisi-after-$NAME"
-echo 'save at '$DIST/"result_meisi-after-$NAME"
+python3 src/visualization/calc_score_meisi.py $ANSWER.wakati.meisi $RE_RECOGNITION_RESULT_PATH".shaped.wakati.meisi" $DIST > $DIST/"result_2meisi-after-$NAME"
+echo 'save at '$DIST/"result_2meisi-after-$NAME"
